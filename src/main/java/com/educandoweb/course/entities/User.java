78ff62752cd17dc6d17a35b8fdd649fb1fe1adea
iararmsantos <iararmsantos@gmail.com>
2019-12-18 20:43:14 -0500

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,15 +2,22 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//mysqlH2
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
-	private String password;
+	private String password;	
 	
 	public User() {
 		
